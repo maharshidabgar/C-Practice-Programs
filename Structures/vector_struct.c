036b74d7f6bce0a0fedda4531d1e693bd4ffee40
maharshidabgar <mahrshidabgar173@gmail.com>
2026-07-06 16:36:25 +0530

    Add vector_struct.c

---
+++ Structures/vector_struct.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+
+struct vector {
+    int x;
+    int y;
+};
+
+void calSum(struct vector v1, struct vector v2, struct vector sum);
+
+int main() {
+
+    struct vector v1 = {5, 10};
+    struct vector v2 = {3, 7};
+    struct vector sum = {0};
+
+    calSum(v1, v2, sum);
+
+    return 0;
+
+}
+
+void calSum(struct vector v1, struct vector v2, struct vector sum) {
+
+    sum.x = v1.x + v2.x;
+    sum.y = v1.y + v2.y;
+
+    printf("Sum of X is : %d\n",sum.x);
+    
+    printf("Sum of Y is : %d\n",sum.y);
+
+}
