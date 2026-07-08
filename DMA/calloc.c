7171178d1d14230a74659daaef4c1e75a1be9ba5
maharshidabgar <mahrshidabgar173@gmail.com>
2026-07-08 12:47:42 +0530

    add calloc.c

---
+++ DMA/calloc.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+   // Runtime Memory Allocation
+int main() {
+
+    float *ptr;
+    ptr = (float *) calloc(5 , sizeof(float));
+
+    // ptr[0] = 15;
+    // ptr[1] = 18;
+    // ptr[2] = 20;
+    // ptr[3] = 25;
+    // ptr[4] = 45;
+
+    for (int i = 0; i < 5; i++) {
+
+        printf("%f\n", ptr[i]);
+
+    }
+
+    return 0;
+
+}
