76b32cbf129d558981ca0f6b1a34fa21c9ef848e
maharshidabgar <mahrshidabgar173@gmail.com>
2026-07-08 11:21:41 +0530

    Add student_InfoFromUser.c

---
+++ FileIO/student_InfoFromUser.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+    FILE *fptr;
+    fptr = fopen("stud.txt","w");
+
+    char name[100];
+    int age;
+    float cgpa;
+
+    printf("Enter Name : ");
+    scanf("%s",&name);
+
+    printf("Enter Age : ");
+    scanf("%d",&age);
+
+    printf("Enter CGPA : ");
+    scanf("%f",&cgpa);
+
+    fprintf(fptr, "Student Name : %s\n", name);
+    fprintf(fptr, "Student Age : %d\n", age);
+    fprintf(fptr, "Student CGPA : %f\n", cgpa);
+
+    fclose(fptr);
+    return 0;
+
+}
